3d6ab29e393074245aaa645b3969be3a633a2c27
WonilLee211 <lwi99556@gmail.com>
2022-09-09 23:51:30 +0900

    220909_TIL

--- Algorithm/Baekjoon/S1_11052.py
+++ Algorithm/Baekjoon/S1_11052.py
@@ -5,5 +5,23 @@ n = int(input())
 # 인덱스 번호와 카드 번호 맞추기
 card_prices = [0] + list(map(int, input().split()))
 
-ppc = [0] + [card_prices[i]/i for i in range(1,n+1)]
-print(ppc)
+ppc = [0] *(n + 1)
+res = n
+total = 0
+
+for i in range(1, n + 1):
+    ppc[i] = card_prices[i]/i 
+
+while res > 0:
+    max_i = res
+    max_p = ppc[max_i]
+
+    for i in range(1, res):
+        if ppc[i] > max_p:
+            max_p = ppc[i]
+            max_i = i
+
+    total += card_prices[max_i]*(res//max_i)
+    res %= max_i
+
+print(int(total))
