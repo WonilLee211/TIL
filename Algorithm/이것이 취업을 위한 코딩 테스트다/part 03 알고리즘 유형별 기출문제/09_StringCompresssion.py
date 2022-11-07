44c3d8ff1642098e213361c0098275d392b96330
WonilLee211 <lwi99556@gmail.com>
2022-11-08 01:55:32 +0900

    221108TIL

--- Algorithm/이것이 취업을 위한 코딩 테스트다/part 03 알고리즘 유형별 기출문제/09_StringCompresssion.py
+++ Algorithm/이것이 취업을 위한 코딩 테스트다/part 03 알고리즘 유형별 기출문제/09_StringCompresssion.py
@@ -3,7 +3,21 @@ import sys
 sys.stdin = open('input.txt')
 
 s = input()
-
+'''
+논리
+- 일단 문자열 길이의 절반 초과의 길이는 압축 단위가 되지 못한다.
+- 압축 단위 1 ~ length // 2
+- 압축 단위별로 
+    - 현재 패턴과 앞 문자가 같을 때
+        - 카운트
+    - 다를 때
+        - comp에 cnt + pattern (cnt == 1, + pattern)
+        - cnt 초기화 pattern 변경
+    - 인덱스 변경
+- 끝값 처리
+    - 마지막에 패턴와 남은 문자열 길이가 딱맞게 안떨어지는 경우 처리
+    
+'''
 length = len(s)
 unit_range = length // 2
 
@@ -16,7 +30,7 @@ for i in range(1, unit_range + 1):
     fr, to = 0, i
     pattern = s[fr:to]
 
-    while to + i < length:
+    while to < length:
 
         if pattern == s[to: to + i]:
             cnt += 1
@@ -27,11 +41,13 @@ for i in range(1, unit_range + 1):
                 compressed += str(cnt) + pattern
             cnt = 1
             pattern = s[to:to + i]
-
         fr, to = to, to + i
-
-    if cnt != 1:
-        compressed += str(cnt) + pattern
+    # 마지막에 패턴의 길이가 딱맞게 안떨어지는 경우
+    if to + i >= length:
+        if cnt != 1:
+            compressed += str(cnt) + pattern
+        else:
+            compressed += pattern + s[to:]
 
     answer = min(answer, len(compressed))
 
