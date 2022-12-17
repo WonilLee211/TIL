eae623c468703e0fc09b3c5c9c89e5b0a954087f
WonilLee211 <lwi99556@gmail.com>
2022-12-17 19:52:42 +0900

    221217_TIL

--- Algorithm/이것이 취업을 위한 코딩 테스트다/part 03 알고리즘 유형별 기출문제/42_boardingGate.py
+++ Algorithm/이것이 취업을 위한 코딩 테스트다/part 03 알고리즘 유형별 기출문제/42_boardingGate.py
@@ -11,13 +11,50 @@ P : 비행기 수 1 <= p < 100,000
 - 도킹할 수 있는 비행기 최대 개수
 
 논리
-- 각 비행기별로 연결될 수 있는 탑승구를 연결리스트로 표현
-
-
+- 서로소 집합 연산 이용
+- 도킹가능한 탑승구 번호에서부터 -1 작은 번호의 탑승구에 대해서 도킹 가능한지 확인
+- 루트가 0이 아니라면 1 ~ 자기 번호까지 도킹할 수 있는 탑승구가 하나는 있다는 뜻
+- 루트가 0이 아니라면 union 
+- 0 이라면 stop
 
 '''
 
 
 import sys
 input = sys.stdin.readline
+
+g = int(input())
+p = int(input())
+
+parent = list(range(g + 1))
+
+def find_parent(x):
+    if x != parent[x]:
+        parent[x] = find_parent(parent[x])
+
+    return parent[x]
+
+def union(x, y):
+    parent[find_parent(y)] = parent[find_parent(x)]
+
+root = 0
+cnt = p
+ans = 0
+
+
+while cnt != 0:
+
+    cnt -= 1
+    info = int(input())
+
+    root = find_parent(info)
+
+    if root == 0:
+        break
+
+    union(parent[info]-1, info)
+    ans += 1
+
+
+print(ans)
 
