98a1265ebc3bacbb23480ba30022d6f86ca20c0c
Wonil Lee <lwi99556@gmail.com>
2023-01-30 23:55:26 +0900

    230130_TIL

--- Algorithm/BOJ/15683_implementation.py
+++ Algorithm/BOJ/15683_implementation.py
@@ -13,6 +13,9 @@ CCTV의 최대 개수는 8개를 넘지 않는다.
 '''
 
 import sys
+import copy
+
+copy = copy.deepcopy
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -29,3 +32,28 @@ cctv = [[],
 idx_cctv = []
 for i in range(n):
     for j in range(m):
+        if office[i][j] not in [0, 6]:
+                idx_cctv.append((i, j))
+
+num_cctv = len(idx_cctv)
+min_acc = 0
+
+
+def mark_cctvline(watchLine, copied_office):
+    for di, dj in watchLine:
+        while True:
+            
+
+    return copied_office
+
+
+def dfs(depth, office):
+    global min_acc
+
+    if depth == num_cctv:
+        min_acc = min(min_acc)
+    else:
+        copied_office = copy(office)
+        for watchLine in cctv[depth]:
+            copied_office = mark_cctvline(watchLine, copied_office)
+            dfs(depth + 1, copied_office)
