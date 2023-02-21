982e65ff3d0364888e80e9cdb4ed7191de25df48
Wonil Lee <lwi99556@gmail.com>
2023-02-21 21:17:58 +0900

    230221_TIL

---
+++ Java/java_springBoot/bitmask_insomnia/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        int oneToNine = (1 << 10) - 1;
+
+        for (int i = 1; i < testcase; i++){
+            int n = Integer.parseInt(br.readLine());
+            int visited = 0;
+
+            for (int count = 0;; count++){
+
+            }
+        }
+    }
+}
