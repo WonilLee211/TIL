e1f1942689a77b91241feb72ef7bf585c3039cfb
valor-lee <lwi99556@gmail.com>
2024-05-09 22:36:01 +0900

    나머지 합 10986 ing

---
+++ C/problem_solving/mod_prefix_sum_10986/mod_prefix_sum_10986/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  mod_prefix_sum_10986
+//
+//  Created by Lee Wonil on 5/9/24.
+// 아이디어 : 누적 값의 나머지를 저장해두고 나머지의 차가 0인 케이스 세기 !
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int mod_refix_sums[N + 1];
+    return 0;
+}
