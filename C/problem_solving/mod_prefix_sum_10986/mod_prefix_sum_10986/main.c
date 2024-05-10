f7a368700a51122e9a0b700069e00e80c74cc76c
valor-lee <lwi99556@gmail.com>
2024-05-10 22:28:49 +0900

    나머지 합 10986 1st trial : 시간초과

--- C/problem_solving/mod_prefix_sum_10986/mod_prefix_sum_10986/main.c
+++ C/problem_solving/mod_prefix_sum_10986/mod_prefix_sum_10986/main.c
@@ -4,6 +4,7 @@
 //
 //  Created by Lee Wonil on 5/9/24.
 // 아이디어 : 누적 값의 나머지를 저장해두고 나머지의 차가 0인 케이스 세기 !
+// 1st : 시간 초과
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,22 @@
 int main(void) {
     int N, M;
     scanf("%d %d", &N, &M);
-    int mod_refix_sums[N + 1];
+    int mod_prefix_sums[N + 1];
+    mod_prefix_sums[0] = 0;
+    
+    int input, res = 0;
+    for (int i = 1; i <= N; i++){
+        scanf("%d", &input);
+        mod_prefix_sums[i] = (input + mod_prefix_sums[i - 1]) % M;
+
+        for (int j = 0; j < i; j++){
+            if ((mod_prefix_sums[i] - mod_prefix_sums[j]) == 0){
+                res++;
+            }
+        }
+        
+    }
+    printf("%d", res);
+    
     return 0;
 }
